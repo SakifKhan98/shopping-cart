791cc5f218613f1a14a655c66ee8972d63f97c18
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-07-27 03:05:04 +0600

    My phone Count

--- script.js
+++ script.js
@@ -16,15 +16,6 @@
 //     document.getElementById("case-total").innerText = "$"+ caseTotal;
 // })
 
-
-document.getElementById("case-increase").addEventListener("click", function() {
-    handleProductChange(true);
-});
-
-document.getElementById("case-decrease").addEventListener("click", function(){
-    handleProductChange(false);
-})
-
 function handleProductChange (isIncrease) {
     const caseInput = document.getElementById("case-count");
     const caseCount = parseInt(caseInput.value);
@@ -41,3 +32,44 @@ function handleProductChange (isIncrease) {
     const caseTotal = caseNewCount * 59;
     document.getElementById("case-total").innerText = "$"+ caseTotal;
 }
+
+// document.getElementById("phone-increase").addEventListener("click", function(){
+//     const phoneInput = document.getElementById("phone-count");
+//     const phoneCount = parseInt(phoneInput.value);
+//     const phoneNewCount = phoneCount +1;
+//     phoneInput.value = phoneNewCount;
+//     const phoneTotal = phoneNewCount * 1219;
+//     document.getElementById("phone-total").innerText = "$" +phoneTotal;
+    
+// })
+
+// document.getElementById("phone-decrease").addEventListener("click", function(){
+//     const phoneInput = document.getElementById("phone-count");
+//     const phoneCount = parseInt(phoneInput.value);
+//     const phoneNewCount = phoneCount -1;
+//     phoneInput.value = phoneNewCount;
+//     const phoneTotal = phoneNewCount * 1219;
+//     document.getElementById("phone-total").innerText = "$" +phoneTotal;
+    
+// })
+
+
+document.getElementById("phone-increase").addEventListener("click", function(){
+    const phoneInput = document.getElementById("phone-count");
+    const phoneCount = parseInt(phoneInput.value);
+    const phoneNewCount = phoneCount +1;
+    phoneInput.value = phoneNewCount;
+    const phoneTotal = phoneNewCount * 1219;
+    document.getElementById("phone-total").innerText = "$" +phoneTotal;
+    
+})
+
+document.getElementById("phone-decrease").addEventListener("click", function(){
+    const phoneInput = document.getElementById("phone-count");
+    const phoneCount = parseInt(phoneInput.value);
+    const phoneNewCount = phoneCount -1;
+    phoneInput.value = phoneNewCount;
+    const phoneTotal = phoneNewCount * 1219;
+    document.getElementById("phone-total").innerText = "$" +phoneTotal;
+    
+})
