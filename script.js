77abb2ca3ab1957ac6a3ff16111e53801c3f3f9e
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-07-27 03:19:10 +0600

    phone case different function added

--- script.js
+++ script.js
@@ -53,23 +53,17 @@ function handleProductChange (isIncrease) {
     
 // })
 
-
-document.getElementById("phone-increase").addEventListener("click", function(){
+function handlePhoneChange (isIncrease){
     const phoneInput = document.getElementById("phone-count");
     const phoneCount = parseInt(phoneInput.value);
-    const phoneNewCount = phoneCount +1;
+    let phoneNewCount = phoneCount;
+    if(isIncrease == true) {
+        phoneNewCount = phoneCount +1;
+    }
+    if(isIncrease == false && phoneCount> 0) {
+        phoneNewCount = phoneCount -1;
+    }
     phoneInput.value = phoneNewCount;
     const phoneTotal = phoneNewCount * 1219;
     document.getElementById("phone-total").innerText = "$" +phoneTotal;
-    
-})
-
-document.getElementById("phone-decrease").addEventListener("click", function(){
-    const phoneInput = document.getElementById("phone-count");
-    const phoneCount = parseInt(phoneInput.value);
-    const phoneNewCount = phoneCount -1;
-    phoneInput.value = phoneNewCount;
-    const phoneTotal = phoneNewCount * 1219;
-    document.getElementById("phone-total").innerText = "$" +phoneTotal;
-    
-})
+}
