ebf6945b060a530cad8b542331fb07842efd1394
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-07-27 03:30:53 +0600

    single function

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+function handleProductChange (product, isIncrease) {
+    const productInput = document.getElementById(product+ "-count");
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
+    if(isIncrease == true){
+        productNewCount = productCount +1;
+    }
+    if(isIncrease == false && productCount >0){
+        productNewCount = productCount -1;
+    }
+    productInput.value = productNewCount;
+    let productTotal = 0;
+    if(product == 'phone'){
+        productTotal = productNewCount * 1219;
+    }
+    if(product == 'case') {
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product+ "-total").innerText = "$"+ productTotal;
+}
+
+
+
+
+
 
 // document.getElementById("case-increase").addEventListener("click", function() {
 //     const caseInput = document.getElementById("case-count");
@@ -16,22 +41,6 @@
 //     document.getElementById("case-total").innerText = "$"+ caseTotal;
 // })
 
-function handleProductChange (isIncrease) {
-    const caseInput = document.getElementById("case-count");
-    const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount;
-    if(isIncrease == true){
-        caseNewCount = caseCount +1;
-
-    }
-    if(isIncrease == false && caseCount >0){
-        caseNewCount = caseCount -1;
-
-    }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById("case-total").innerText = "$"+ caseTotal;
-}
 
 // document.getElementById("phone-increase").addEventListener("click", function(){
 //     const phoneInput = document.getElementById("phone-count");
@@ -53,17 +62,35 @@ function handleProductChange (isIncrease) {
     
 // })
 
-function handlePhoneChange (isIncrease){
-    const phoneInput = document.getElementById("phone-count");
-    const phoneCount = parseInt(phoneInput.value);
-    let phoneNewCount = phoneCount;
-    if(isIncrease == true) {
-        phoneNewCount = phoneCount +1;
-    }
-    if(isIncrease == false && phoneCount> 0) {
-        phoneNewCount = phoneCount -1;
-    }
-    phoneInput.value = phoneNewCount;
-    const phoneTotal = phoneNewCount * 1219;
-    document.getElementById("phone-total").innerText = "$" +phoneTotal;
-}
+
+
+
+// function handleProductChange (isIncrease) {
+//     const caseInput = document.getElementById("case-count");
+//     const caseCount = parseInt(caseInput.value);
+//     let caseNewCount = caseCount;
+//     if(isIncrease == true){
+//         caseNewCount = caseCount +1;
+//     }
+//     if(isIncrease == false && caseCount >0){
+//         caseNewCount = caseCount -1;
+//     }
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+//     document.getElementById("case-total").innerText = "$"+ caseTotal;
+// }
+
+// function handlePhoneChange (isIncrease){
+//     const phoneInput = document.getElementById("phone-count");
+//     const phoneCount = parseInt(phoneInput.value);
+//     let phoneNewCount = phoneCount;
+//     if(isIncrease == true) {
+//         phoneNewCount = phoneCount +1;
+//     }
+//     if(isIncrease == false && phoneCount> 0) {
+//         phoneNewCount = phoneCount -1;
+//     }
+//     phoneInput.value = phoneNewCount;
+//     const phoneTotal = phoneNewCount * 1219;
+//     document.getElementById("phone-total").innerText = "$" +phoneTotal;
+// }
