c326599cbee00d25713dd97a392c17c961940fe0
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-07-27 04:12:34 +0600

    Tax & Grand Total Functionality

--- script.js
+++ script.js
@@ -17,11 +17,30 @@ function handleProductChange (product, isIncrease) {
         productTotal = productNewCount * 59;
     }
     document.getElementById(product+ "-total").innerText = "$"+ productTotal;
+    calculateTotal();
 }
 
+function calculateTotal() {
+    const phoneCount = getInputValue('phone');
+    const caseCount = getInputValue('case');
+
+    const totalPrice = phoneCount * 1219 + caseCount* 59;
+    document.getElementById("total-price").innerText = "$" + totalPrice;
+
+    const tax = Math.round(totalPrice * 0.1);
+    document.getElementById("tax-amount").innerText = "$" + tax;
+
+    const grandTotal = totalPrice + tax;
+    document.getElementById("grand-total"). innerText = "$" + grandTotal;
 
 
+}
 
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
 
 
 // document.getElementById("case-increase").addEventListener("click", function() {
